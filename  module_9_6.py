954ced35173e0edd74c435c77ec41c7dba69dd83
captainBashOrg <captain@sinn.ru>
2024-09-15 20:53:02 +0300

    Add files via upload

---  module_9_6.py
+++  module_9_6.py
@@ -3,10 +3,14 @@ print("Генераторы")
 def all_variants(text):
     x = 0
     y = 0
-    for x in range(len(text)):
-        for y in range(x + 1, len(text) + 1):
-            #print(text[x:y])
-            yield text[x:y]
+#    for x in range(len(text)):
+#        for y in range(x + 1, len(text) + 1):
+#            #print(text[x:y])
+#            yield text[x:y]
+
+    for length in range(len(text)):
+        for l in range(len(text)-length):
+            yield text[l:l+length+1]
 
 a = all_variants("abcdef")
 for i in a:
